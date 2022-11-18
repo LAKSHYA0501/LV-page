7d049ef8f11dcf1e307e76abd4bf162eaa26e973
LAKSHYA0501 <lakshya.varshney_iot18@gla.ac.in>
2022-11-18 22:43:11 +0530

    minor changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>RING EMPORIUM</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="https://kit.fontawesome.com/a02b2830e8.js" crossorigin="anonymous"></script>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
